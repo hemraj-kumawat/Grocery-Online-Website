8aee6e745eab8ac2192ae28b0bb860217c921285
Hemraj Kumawat <hemukumarawat689@gmail.com>
2026-02-23 09:00:27 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Grocery-Online-Website/",
   plugins: [react(),tailwindcss(),],
 })
